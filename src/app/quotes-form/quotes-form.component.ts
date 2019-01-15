421884f4793bbcd6245eea4e913de239ae552c09
kelvinadeu <kelvinadeu25@gmail.com>
2019-01-15 14:22:23 +0300

    add to app

--- src/app/quotes-form/quotes-form.component.ts
+++ src/app/quotes-form/quotes-form.component.ts
@@ -7,7 +7,7 @@ import {Quotes} from '../quotes';
 })
 export class QuotesFormComponent implements OnInit {
 
-  newQuotes=new Quotes(0,"","",new Date());
+  newQuotes=new Quotes(0,"","","",new Date());
 @Output() addQuotes=new EventEmitter<Quotes>();
 
 
